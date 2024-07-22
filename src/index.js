7f4e33d4cc764fea574e97021e092c4d3dedff5e
TaraWalk <tarawalker1310@gmail.com>
2024-07-22 10:11:10 +0100

    Added Basic JS and Typewriter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: "The Poem",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+
+  poemElement.innerHTML = "the Poem";
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
