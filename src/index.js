d12786ea2521edf888bf146eea2a4f670df188f1
TaraWalk <tarawalker1310@gmail.com>
2024-07-22 10:41:55 +0100

    Added AI API integration

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "The Poem",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+function generatePoem(event) {
+  event.preventDefault();
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "62fb8e28a4eab4a583ea42bf3ofat058";
+  let prompt = `User Instructions: Generate a Harry Potter poem about ${instructionsInput.value}`;
+  let context =
+    "You are a fantasy book reader, generate a 4 line poem in basic HTML, please make sure to follow user instructions";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  poemElement.innerHTML = "the Poem";
+  axios.get(apiURL).then(displayPoem);
+
+  //poemElement.innerHTML = "the Poem";
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
